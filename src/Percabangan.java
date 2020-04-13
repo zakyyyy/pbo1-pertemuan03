1efd5a11be7a0401b833637241df5d0b5e4fa84c
zaky <mzaky2678@gmail.com>
2020-04-13 18:02:54 +0800

    if else (4)

--- src/Percabangan.java
+++ src/Percabangan.java
@@ -5,7 +5,7 @@ public class Percabangan {
 
         if(uangDiDompet>totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
-        }else if(uangDiDompet<totalBelanja) {
+        }else if(uangDiDompet>totalBelanja) {
             double angsul = uangDiDompet-totalBelanja;
             System.out.println("Uang cukup,angsul : "+angsul);
         }else {
