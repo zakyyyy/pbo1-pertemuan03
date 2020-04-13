5f412cefac9ac7a9e7339049c2ee57936d01d98b
zaky <mzaky2678@gmail.com>
2020-04-13 18:11:38 +0800

    Switch (1)

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+    public static void main(String[] args) {
+      int jalanJalan=5;
+      switch (jalanJalan){
+          case 1:
+              System.out.println("urang lapas");
+              break;
+          case 2:
+              System.out.println("urang acayan");
+              break;
+          case 3:
+              System.out.println("ubat nyamuk");
+              break;
+          case 4:
+              System.out.println("dubil dit");
+              break;
+          case 5:
+              System.out.println("makluk halus");
+              break;
+      }
+    }
+}
