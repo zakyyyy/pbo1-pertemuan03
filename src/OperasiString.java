0ca65b011b41d81abf5ecbc879c101326be530a3
zaky <mzaky2678@gmail.com>
2020-04-13 17:08:25 +0800

    toUpperCase untuk. . . toLowerCase untuk. . . substring(5) untuk. . . substring(0,4) untuk. . .

--- src/OperasiString.java
+++ src/OperasiString.java
@@ -3,10 +3,15 @@ public class OperasiString {
         String kota = "Yogyakarta";
 
         System.out.println(kota);
-        
+
         char[] uniskaChar ={ 'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
 
     }
 }
