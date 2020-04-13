c88f2d045123cbc5fcb057e1cd9f6676662bbcc2
zaky <mzaky2678@gmail.com>
2020-04-13 17:51:25 +0800

    if else (1)

---
+++ src/Percabangan.java
@@ -0,0 +1,13 @@
+public class Percabangan {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if(uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else {
+            System.out.println("Uang cukup,Beralaan lah");
+        }
+
+    }
+}
