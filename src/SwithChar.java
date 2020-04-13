1d9458220dba368b19863cc62f17f52987e8ba23
zaky <mzaky2678@gmail.com>
2020-04-13 18:25:36 +0800

    Switch (2)

---
+++ src/SwithChar.java
@@ -0,0 +1,29 @@
+public class SwithChar {
+    public static void main(String[] args) {
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs     : \"Trimkasih pak\"");
+                System.out.println("Dosen     : \"Selamat ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs     : \"Kenapa ulun kada dapat A,pak\"");
+                System.out.println("Dosen   : \"kurang hoki\"");
+                break;
+            case 'C':
+                System.out.println("Mhs     : \"Ulun turun full pak ai,Tugas full jua\"");
+                System.out.println("Dosen   : \"Tapi dapat lah menjawab ujian\"");
+                System.out.println("Mhs     : \"wkwkwk...\"");
+                break;
+            case 'D':
+                System.out.println("Mhs     : \"Ulun turun full pak ai,Tugas full jua\"");
+                System.out.println("Dosen   : \"Bujur jua kah\"");
+                System.out.println("Mhs     : \"Memeriksa berkas\"");
+                System.out.println("Dosen   : \"Senyum\"");
+                System.out.println("Mhs     : \"Alhamdulillah\"");
+                break;
+
+        }
+    }
+}
+
